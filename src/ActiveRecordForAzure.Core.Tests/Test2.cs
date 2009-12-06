1b410103d07bba306d16163470f236e8fcaa73fd
chrisl <christofer.lof@gmail.com>
2009-12-06 20:09:25 +0100

    test

--- src/ActiveRecordForAzure.Core.Tests/Test2.cs
+++ src/ActiveRecordForAzure.Core.Tests/Test2.cs
@@ -2,8 +2,14 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Xunit;
 
 namespace ActiveRecordForAzure.Core.Tests {
     class Test2 {
+
+        [Fact]
+        public void FactMethodName() {
+            
+        }
     }
 }
