bb1a2bfe5238226eaf3327054d2bd9bf54c3c3e6
chrisl <christofer.lof@gmail.com>
2009-12-06 20:02:49 +0100

    initial load

---
+++ src/ActiveRecordForAzure.Core.Tests/Test.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xunit;
+
+namespace ActiveRecordForAzure.Core.Tests {
+    public class Test {
+
+        [Fact]
+        public void FactMethodName() {
+            Assert.Equal(true,false);
+        }
+    }
+}
